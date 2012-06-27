db30c39851acf78eea0bcec596b5ebb128ce2df6
Eric Schulte <eric.schulte@gmx.com>
2012-06-26 18:06:54 -0600

    simplified

--- fft/fft.ino
+++ fft/fft.ino
@@ -11,35 +11,16 @@ char data[(NUM_FREQS*2)];
 int pin_adc = 0;
 
 void setup() {
-  // initialize the digital pin as an output.
-  // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(13, OUTPUT);
   Serial.begin(9600);
 }
 
 void send_data(char *data){
-  int i, val;
-  int total = 0;
+  int i;
   for(i=0;i<NUM_FREQS;++i){
-    val = (int)data[i];
-    // if(val>99){
-    //   Serial.print(1);
-    //   val -= 100;
-    // } else {
-    //   Serial.print(" ");
-    // }
-    // if(val>9){
-    //   Serial.print(1);
-    //   val -= 10;
-    // } else {
-    //   Serial.print(" ");
-    // }
-    Serial.print(val);
-    Serial.print(" ");
+    Serial.print((int)data[i]);
+    if(i<(NUM_FREQS-1)) Serial.print(" ");
   }
-  for(i=0;i<NUM_FREQS;i++) total += data[i];
-  Serial.print("| ");
-  Serial.println(total);
+  Serial.println("");
 }
 
 void loop(){
@@ -54,18 +35,13 @@ void loop(){
       data[i] = val / 4 - 128;
       im[i] = 0;
       i++;
-    }
-    else{
-      //this could be done with the fix_fftr function without the im array.
+    } else {
       fix_fft(data, im, 7, 0);
-      // I am only interessted in the absolute value of the transformation
       for (i=0; i<NUM_FREQS;i++){
         data[i] = sqrt(data[i] * data[i] + im[i] * im[i]);
       }
-      // do something with the data values 1..64 and ignore im
       send_data(data);
     }
-
     tt = millis();
   }
 }
